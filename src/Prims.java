131baf330d025e7a9f3bf069aaeeee21c4871b1a
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-08-05 12:56:20 -0400

    h

--- src/Prims.java
+++ src/Prims.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Primitives {
+public class Prims {
+
+    public static void main(String[] args) {
+        //Integer Types
+
+        int myInt = 7;      //32-bit integer or an 4 byte integer.  2 ^ 32 different values
+        System.out.println(myInt);
+        short myShort = 8;  //16-bit integer of an 2 byte integer. 2 ^16 different values
+        System.out.println(myShort);
+        byte myByte = 9;  //8-bit integer.  2^8 = 256 values.  -128 <-> 127
+        System.out.println(myByte);
+        long myLong = 51558469663635251L;
+        System.out.println(myLong);
+
+
+        String myString = "Hello World";
+        char myChar = 'a';  //16-bits
+
+        boolean myBoolean = true;  //8 bits
+        boolean myBoolean2 = false;
+
+        Integer myInt2 = 7;      //32-bit integer or an 4 byte integer.  2 ^ 32 different values
+        System.out.println(myInt);
+        Short myShort2 = 8;  //16-bit integer of an 2 byte integer. 2 ^16 different values
+        System.out.println(myShort);
+        Byte myByte2 = 9;  //8-bit integer.  2^8 = 256 values.  -128 <-> 127
+        System.out.println(myByte);
+        Long myLong2 = 51558469663635251L;
+        System.out.println(myLong);
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE + 1);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Character.MIN_VALUE);
+        System.out.println(Character.MAX_VALUE);
+
+
+
+
+
+
+    }
+
+
 }
